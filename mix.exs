24aaed9bbe4511a4bc38b856863ae5d0fafb76f3
Jaremy Creechley <creechley@gmail.com>
2020-07-26 20:07:26 -0600

    update mixes

--- mix.exs
+++ mix.exs
@@ -1,28 +1,76 @@
-defmodule ToolchainExtrasBlas.MixProject do
+defmodule NervesPruIcss.MixProject do
   use Mix.Project
+
+  @app :toolchain_extras_blas
 
   def project do
     [
-      app: :toolchain_extras_blas,
+      app: @app,
+      name: "toolchain_extras_blas",
       version: "0.1.0",
-      elixir: "~> 1.10",
-      start_permanent: Mix.env() == :prod,
-      deps: deps()
+      elixir: "~> 1.8",
+      compilers: [:nerves_package] ++ Mix.compilers(),
+      nerves_package: nerves_package(),
+      description: "A package for OpenBLAS",
+      deps: deps(),
+      package: package(),
+      aliases: [loadconfig: [&bootstrap/1]],
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
-  def application do
+  def nerves_package do
     [
-      extra_applications: [:logger]
+      name: @app,
+      type: :extras_toolchain,
+      platform: NervesExtras.Toolchain,
+      toolchain_extras: [
+        env_var: "BLAS",
+        build_path_link: "openblas",
+        build_script: "build.sh",
+        clean_files: ["openblas"],
+        archive_script: "scripts/archive.sh"
+      ],
+      platform_config: [],
+      artifact_sites: [
+        {:github_releases, "elcritch/#{@app}"}
+      ],
+      checksum: package_files()
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+  def application do
+    []
+  end
+
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:nerves, "~> 1.5", runtime: false},
+      {:toolchain_extras, "~> 0.2", runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Jaremy Creechley"],
+      files: package_files(),
+      licenses: ["Apache 2.0"],
+      links: %{"Github" => "https://github.com/elcritch/#{@app}"}
+    ]
+  end
+
+  defp package_files do
+    [
+      "README.md",
+      "LICENSE",
+      "mix.exs",
+      "lib",
+      "config"
+    ]
+  end
+
+  defp bootstrap(args) do
+    Application.start(:nerves_bootstrap)
+    Mix.Task.run("loadconfig", args)
   end
 end
